ebe925f5cd599a0be428cb445df9be77685c685c
junghunim07 <dlawndgns9@naver.com>
2023-10-10 20:06:27 +0900

    Mission1.3

--- Mission3.java
+++ Mission3.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
-public class Main {
+public class Mission3 {
     static int num;
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
